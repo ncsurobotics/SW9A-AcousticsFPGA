d001982cada4fb207429c1d5d8c16848ca37a8ae
Anirudh_Bhaskar <baniru007@gmail.com>
2025-10-15 20:39:15 -0400

    Update command_reader.v

--- command_reader.v
+++ command_reader.v
@@ -10,35 +10,38 @@ module command_reader (
   input [4:0] bartlett_addr,
   input bartlett_wr_en,
   input [31:0] bartlett_data_in,
-  output [7:0] threshold,
-  output [7:0] frequency,
-  output [7:0] vga,
+  output [31:0] threshold,
+  output [31:0] frequency,
+  output [31:0] vga,
   output dumpram,
   output sendmaxangle,
-  output softreset
+  output softreset,
+  output [31:0] maxangle,
+  output reg [31:0] bartlett_flipflops [0:19]
 );
 
-  // do the actual register file with 26 of them and each is 32 bits
-  reg [31:0] flipflops [0:25];
+  reg [31:0] uart_flipflops [20:25];
 
   // to write the data to the flipflops
+  // uart
   integer i;
   always @(posedge clk or negedge rst_n) begin  
     if (!rst_n) begin
       for (i = 20; i <= 25; i = i + 1)
-        flipflops[i] <= 0;
+        uart_flipflops[i] <= 0;
     end else if (uart_wr_en && uart_addr >= 20 && uart_addr <= 25) begin
-      flipflops[uart_addr] <= uart_data_in;
+      uart_flipflops[uart_addr] <= uart_data_in;
     end
   end
   
+  // bartlett
   integer j;
   always @(posedge clk or negedge rst_n) begin  
     if (!rst_n) begin
       for (j = 0; j <= 19; j = j + 1)
-        flipflops[j] <= 0;
+        bartlett_flipflops[j] <= 0;
     end else if (bartlett_wr_en && bartlett_addr <= 19) begin
-      flipflops[bartlett_addr] <= bartlett_data_in;
+      bartlett_flipflops[bartlett_addr] <= bartlett_data_in;
     end
   end
 
@@ -46,16 +49,16 @@ module command_reader (
   // uart
   always @(*) begin
     if (uart_addr >= 20 && uart_addr <= 25)
-      uart_data_out = flipflops[uart_addr];  
+      uart_data_out = uart_flipflops[uart_addr];  
     else
       uart_data_out = 32'hzzzzzzzz;  // invalid read
   end
 
-  assign threshold     = flipflops[20][7:0];
-  assign frequency     = flipflops[21][7:0];
-  assign vga           = flipflops[22][7:0];
-  assign dumpram       = flipflops[23][0];  // hardwired output for dump ram
-  assign sendmaxangle  = flipflops[24][0];  // hardwired output for max angle
-  assign softreset     = flipflops[25][0];  // hardwired output for soft reset
+  assign threshold     = uart_flipflops[20][7:0];
+  assign frequency     = uart_flipflops[21][7:0];
+  assign vga           = uart_flipflops[22][7:0];
+  assign dumpram       = uart_flipflops[23][0];  // hardwired output for dump ram
+  assign sendmaxangle  = uart_flipflops[24][0];  // hardwired output for max angle
+  assign softreset     = uart_flipflops[25][0];  // hardwired output for soft reset
 
 endmodule
